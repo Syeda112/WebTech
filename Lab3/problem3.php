f34efafc967bf73b4295fc9accd461ba6a9af3a9
Syeda112 <israt.jahan3058@gmail.com>
2021-02-08 13:56:17 +0600

    Lab3

---
+++ Lab3/problem3.php
@@ -0,0 +1,14 @@
+<?php
+$a = 3;
+$b = 3;
+echo "Area = ";
+echo $a*$b;
+echo"<br>";
+echo "perimeter = ";
+echo 2* ($a+$b);
+echo"<br>";
+
+if($a==$b) {
+echo "the shape is square";
+}
+?>
---
+++ Lab3/problem5.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$backColor="green"; $fontColor="red";$imageWidth"80px"; $imageHeight="80px";
+?>
+<img src"aiub_logo.jpg" width ="<?php echo $imageWidth ?>" hieght="<?php echo $imageHeight ?>" />
+<h2>American International University - <span style= "background-color:<?php echo $backcolor?> ; color:<?php echo $fontcolor?>" >Bangladesg</h2>
+</span> 
+
+.
+
+</body>
+</html>
